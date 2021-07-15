0e03cc4f66fe4d9f98c9d478c95a6978364bdd9c
Tomer <Lilush>
2021-07-15 15:55:32 +0300

    added ll-hostname and collection-token inputs

--- index.js
+++ index.js
@@ -4,21 +4,22 @@ const got = require('got');
 const fs = require('fs')
 
 try {
+    const hostname = core.getInput('ll-hostname')
     const terraformPlanPath = core.getInput('plan-json');
     const plan = JSON.parse(fs.readFileSync(terraformPlanPath, 'utf8'))
 
-    const url = "https://mikelytics.lightops.io/api/v1/collection/terraform"
+    const url = `https://${hostname}/api/v1/collection/terraform`
     const headers = {
-        'X-Lightlytics-Token': 'LKGrQlbyzVqtorTZ4LYJxo267DAZwDUTtqS9j8se9xc'
+        'X-Lightlytics-Token': core.getInput('collection-token')
     }
 
     const source = {
-        format: 'Terraform',
+        name: 'Github',
         type: 'Github',
+        format: 'Terraform',
         branch: github.context.payload.pull_request.head.ref,
         base_branch: github.context.payload.pull_request.base.ref,
         commit_hash: github.context.payload.pull_request.head.sha,
-        name: 'Github',
         pr_id: github.context.payload.pull_request.number,
         repository: github.context.payload.repository.full_name,
         user_name: github.context.payload.pull_request.user.login
@@ -29,15 +30,12 @@ try {
         metadata: {source},
     }
 
-    const {body} = got.post(url, {
+    got.post(url, {
         json: data,
         responseType: 'json',
         headers
     }).then(() => {
     }).catch(err => console.log(err));
-
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
 } catch (error) {
     core.setFailed(error.message);
 }
